2d614ecc4fd7db78fd7998240460fc7eefe48a70
OmkoBass <omkob@hotmail.com>
2021-10-16 16:24:01 +0200

    Moved the changing background to only the home page

--- dispense-the-thought/src/App.js
+++ dispense-the-thought/src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import { MantineProvider } from "@mantine/core";
 
-import { motion } from "framer-motion";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./views/Home";
@@ -14,24 +12,15 @@ function App() {
   return (
     <MantineProvider>
       <Router>
-        <motion.div
-          animate={{ backgroundColor: ["#acc6f0", "#fac4b0", "#acc6f0"] }}
-          transition={{
-            easings: ["easeIn"],
-            repeat: Infinity,
-            duration: 10,
-          }}
-        >
-          <Header />
-          <Switch>
-            <Route path="/thoughts">
-              <Thoughts />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </motion.div>
+        <Header />
+        <Switch>
+          <Route path="/thoughts">
+            <Thoughts />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </Router>
     </MantineProvider>
   );
