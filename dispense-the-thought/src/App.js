16a19fc235483a648648c66192bec0826d3dbc4f
OmkoBass <omkob@hotmail.com>
2021-10-16 13:34:15 +0200

    Added background color transitions

--- dispense-the-thought/src/App.js
+++ dispense-the-thought/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { MantineProvider } from "@mantine/core";
 
+import { motion } from "framer-motion";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./views/Home";
@@ -12,15 +14,24 @@ function App() {
   return (
     <MantineProvider>
       <Router>
-        <Header />
-        <Switch>
-          <Route path="/thoughts">
-            <Thoughts />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <motion.div
+          animate={{ backgroundColor: ["#acc6f0", "#fac4b0", "#acc6f0"] }}
+          transition={{
+            easings: ["easeIn"],
+            repeat: Infinity,
+            duration: 10,
+          }}
+        >
+          <Header />
+          <Switch>
+            <Route path="/thoughts">
+              <Thoughts />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </motion.div>
       </Router>
     </MantineProvider>
   );
