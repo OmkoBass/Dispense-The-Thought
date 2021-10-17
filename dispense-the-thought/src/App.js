97ffacfe8a48a9be2164f97731e4b175c1aa27aa
OmkoBass <omkob@hotmail.com>
2021-10-17 22:13:22 +0200

    Changed the default loader

--- dispense-the-thought/src/App.js
+++ dispense-the-thought/src/App.js
@@ -10,7 +10,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <MantineProvider>
+    <MantineProvider theme={{ loader: "bars" }}>
       <Router>
         <Header />
         <Switch>
